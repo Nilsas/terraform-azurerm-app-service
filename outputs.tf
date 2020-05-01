4030eb6f4621ff491a811cc71077c067708c4ff0
Nilsas <n.firantas@gmail.com>
2020-05-01 20:22:36 +0300

    return all resources outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
+output "app_service_plan" {
+  value = azurerm_app_service_plan.asp
+}
+
 output "app_service" {
   value = azurerm_app_service.prod
+}
+
+output "app_service_certificate" {
+  value = azurerm_app_service_certificate
+}
+
+output "app_service_custom_hostname_binding" {
+  value = azurerm_app_service_custom_hostname_binding.cert_bind
 }
